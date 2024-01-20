99fc7662854f47b57a7c714f97edbed0affdca20
Oat-Dev <oat.1141@gmail.com>
2024-01-20 21:19:04 +0700

    fix: response status and message authToken

--- app/api/helper/authenticateToken.js
+++ app/api/helper/authenticateToken.js
@@ -5,12 +5,12 @@ const authenticateToken = (req, res, next) => {
   const token = authHeader && authHeader.split(' ')[1];
 
   if (token == null) {
-    return res.sendStatus(401);
+    return res.status(401).json({ status: 401, message: "Unauthorized Token" });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return res.sendStatus(403);
+      return res.status(403).json({ status: 403, message: "Forbidden Token" });
     }
     req.user = user;
     next();
