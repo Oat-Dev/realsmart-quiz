a20da804b642155ebdf5188a24c45cef23b26c15
Oat-Dev <oat.1141@gmail.com>
2024-01-20 21:28:29 +0700

    fix: authToken edit .env key

--- app/api/helper/authenticateToken.js
+++ app/api/helper/authenticateToken.js
@@ -8,7 +8,7 @@ const authenticateToken = (req, res, next) => {
     return res.status(401).json({ status: 401, message: "Unauthorized Token" });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.jwtSecretKey, (err, user) => {
     if (err) {
       return res.status(403).json({ status: 403, message: "Forbidden Token" });
     }
